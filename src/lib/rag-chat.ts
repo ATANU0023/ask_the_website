3ad9812c752b027a0f74df2052c878c43f565318
Atanu Pramanik <atanupramanik506@gmail.com>
2025-06-05 13:04:58 +0530

    Update environment variables for RAGChat configuration

--- src/lib/rag-chat.ts
+++ src/lib/rag-chat.ts
@@ -1,7 +1,9 @@
-import { RAGChat, upstash } from "@upstash/rag-chat"
+import { groq, RAGChat, upstash } from "@upstash/rag-chat"
 import { redis } from "./redis"
 
 export const ragChat = new RAGChat({
-    model: upstash("meta-llama/Meta-Llama-3-8B-Instruct"),
+    model: groq("meta-llama/llama-4-scout-17b-16e-instruct",{
+        apiKey: process.env.GROQ_KEY
+    }),
     redis: redis,
 })
